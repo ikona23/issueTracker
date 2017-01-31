2a1e6a283725996ca129a674f77352351908ad48
ikona23 <ikona23@gmail.com>
2017-01-31 20:05:51 +1300

    form is working

--- js/app.js
+++ js/app.js
@@ -1,13 +1,21 @@
 document.getElementById('issueInputForm').addEventListener('submit',saveIssue)
 
+function createdTime(){
+  var d = new Date()
+  return d
+  console.log(d);
+}
+
 function saveIssue(e) {
   var issueDesc = document.getElementById('issueDescInput').value
+  var issueTimeAtTo = document.getElementById('createdTime').value
   var issueSeverity = document.getElementById('issueSeverityInput').value
   var issueAssignedTo = document.getElementById('issueAssignedToInput').value
   var issueId = chance.guid()
   var issueStatus = 'Open'
 
   var issue = {
+    createdTime: issueTimeAtTo,
     id: issueId,
     description: issueDesc,
     severity: issueSeverity,
@@ -30,6 +38,11 @@ document.getElementById('issueInputForm').reset()
   e.preventDefault()
 }
 
+
+
+document.getElementById('createdTime')
+console.log(createdTime());
+
 function fetchIssues() {
   var issues = JSON.parse(localStorage.getItem('issues'))
   var issuesList = document.getElementById('issuesList')
@@ -42,10 +55,18 @@ function fetchIssues() {
     var severity = issues[i].severity
     var assignedTo = issues[i].assignedTo
     var status = issues[i].status
+    var createdAt = issues[i].createdAt
+// ?/
 
-
-    issuesList.innerHTML += '<div class="well">' +
-                            '<h6>Issue ID:' + id + '</h6>'+ '<p> <span class = "label label-info">' + status + '</span></p>' + '<h3>' + desc + '</h3>' + '<p><span class="glyphicon-time"></span>' + severity + '</p>' + '<p><span class="glyphicon-user"></span>' + assignedTo + '<p>' + '<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning">Close</a>' + '<a href="#" onclick="deleteIssue(\''+id+'\')" class="btn btn-danger">Delete</a>'+
-'</div>'
-  }
+issuesList.innerHTML +=   '<div class="well">'+
+                          '<h6>Issue ID: ' + id + '</h6>'+
+                          '<h6>Time Created: ' + createdAt + '</h6>'+
+                          '<p><span class="label label-info">' + status + '</span></p>'+
+                          '<h3>' + desc + '</h3>'+
+                          '<p><span class="glyphicon glyphicon-time"></span> ' + severity + '</p>'+
+                          '<p><span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>'+
+                          '<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning">Close</a> '+
+                          '<a href="#" onclick="deleteIssue(\''+id+'\')" class="btn btn-danger">Delete</a>'+
+                          '</div>';
+}
 }
